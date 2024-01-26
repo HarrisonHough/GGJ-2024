0d46b1eeb8ae569a76d1bbe43541d4d3ef53b90d
Harrison Hough <harrisonhough4@gmail.com>
2024-01-27 00:12:02 +0200

    started on prompt trigger

--- Assets/Scripts/UI/PromptPanel.cs
+++ Assets/Scripts/UI/PromptPanel.cs
@@ -4,12 +4,25 @@ public class PromptPanel : MonoBehaviour
 {
     [SerializeField] private string promptText;
     [SerializeField] private ResponseButton[] button;
+    [SerializeField] private GameObject panel;
+    
+    private void Start()
+    {
+        PromptTrigger.OnPromptTriggered += LoadPrompt;
+        
+    }
     
     public void LoadPrompt(PromptData promptData)
     {
-        this.promptText = promptData.promptText;
-        for (int i = 0; i < promptData.responses.Length; i++)
+        panel.SetActive(false);
+        promptText = promptData.promptText;
+        for (var i = 0; i < button.Length; i++)
         {
+            if (i >= promptData.responses.Length)
+            {
+                button[i].gameObject.SetActive(false);
+                return;
+            }
             button[i].SetText(promptData.responses[i].Text);
         }
     }
