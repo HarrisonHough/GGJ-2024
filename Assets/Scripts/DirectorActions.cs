0603ef1347847e660692afde5c2ec0e341a23417
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-01-27 17:56:18 +0200

    director script

---
+++ Assets/Scripts/DirectorActions.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DirectorActions : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private Animator animator;
+    
+    [SerializeField] private AudioClip[] goodLines;
+    [SerializeField] private AudioClip[] neutralLines;
+    [SerializeField] private AudioClip[] badLines;
+
+    private Dictionary<LineType, (AudioClip[], string)> lines;
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        
+        lines = new Dictionary<LineType, (AudioClip[], string)>
+        {
+            {LineType.Good, (goodLines, "Good")},
+            {LineType.Neutral, (neutralLines, "Neutral")},
+            {LineType.Bad, (badLines, "Bad")}
+        };
+    }
+    
+    public void PlayLine(LineType lineType)
+    {
+        var (clips, trigger) = lines[lineType];
+        var clip = clips[Random.Range(0, clips.Length)];
+        
+        audioSource.PlayOneShot(clip);
+        animator.SetTrigger(trigger);
+    }
+}
---
+++ Assets/Scripts/LineType.cs
@@ -0,0 +1,6 @@
+public enum LineType
+{
+    Good,
+    Neutral,
+    Bad
+}
