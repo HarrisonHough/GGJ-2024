c892119f5b8d0715215b96cbbda06cfa10a106b4
Harrison <harrisonhough4@gmail.com>
2024-01-28 00:05:48 +0200

    added screen fader and audio

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
-using System;
+using System.Collections;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public enum GameState
 {
@@ -15,17 +17,23 @@ public class GameManager : MonoBehaviour
     public GameState GameState {get; private set; } = GameState.Playing;
     [SerializeField] private CinemachineBrain cinemachineBrain;
     [SerializeField] private CinemachineVirtualCamera mainCamera;
-
+    [SerializeField] private AudioClip goodEndingClip;
+    [SerializeField] private AudioClip neutralEndingClip;
+    [SerializeField] private AudioClip badEndingClip;
+    [SerializeField] private AudioClip introClip;
+    [SerializeField] private ScreenFader screenFader;
+    private AudioSource audioSource;
     private float playerScore;
     public const float SCORE_TARGET = 8;
-    public static Action OnGameOver;
-    public static Action OnScoreReached;
     private int numberOfResponses;
+    private const string ENDING_PREF = "ENDING_INDEX";
     
     private void Start()
     {
-        SetGameState(GameState.Playing);
+        audioSource = GetComponent<AudioSource>();
+        SetGameState(GameState.Paused);
         DirectorActions.OnDirectorResponse += AfterDirectorResponse;
+        StartCoroutine(WaitForStart());
     }
 
     private void AfterDirectorResponse()
@@ -52,19 +60,50 @@ public class GameManager : MonoBehaviour
         playerScore = Mathf.Clamp(playerScore, 0f,SCORE_TARGET);
         if (playerScore >= SCORE_TARGET)
         {
-            Debug.Log("You win!");
-            OnGameOver?.Invoke();
+            PlayerPrefs.SetInt(ENDING_PREF, 0);
+            StartCoroutine(WaitAndFinish(goodEndingClip));
+            return;
         }
-        if(playerScore <=  0 || numberOfResponses >= 12)
+        if( numberOfResponses >= 12)
         {
-            Debug.Log("You lose!");
-            OnGameOver?.Invoke();
+            PlayerPrefs.SetInt(ENDING_PREF, 1);
+            StartCoroutine(WaitAndFinish(neutralEndingClip));
+            return;
         }
+        if(playerScore <=  0 )
+        {
+            PlayerPrefs.SetInt(ENDING_PREF, 2);
+            StartCoroutine(WaitAndFinish(badEndingClip));
+        }    
     }
 
     
     public void SetGameState(GameState gameState)
     {
         GameState = gameState;
+    }
+    
+    private IEnumerator WaitForStart()
+    {   
+        PlayAudio(introClip);
+        screenFader.FadeFromBlack(2f);
+        yield return new WaitForSeconds(introClip.length);
+        SetGameState(GameState.Playing);
+    }
+
+    private void PlayAudio(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;  
+        audioSource.Play();
+    }
+
+    private IEnumerator WaitAndFinish(AudioClip audioClip)
+    {
+        PlayAudio(audioClip);
+        yield return new WaitForSeconds(audioClip.length);
+        var duration = 1f;
+        screenFader.FadeToBlack(duration);
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadScene(2);
     }
 }
---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour
+{
+    private Image blackScreenImage;
+    private bool isFading = false;
+
+    private void Start()
+    {
+        blackScreenImage = GetComponent<Image>();
+        blackScreenImage.color = new Color(0, 0, 0, 0);
+    }
+
+    public void FadeToBlack(float fadeDuration)
+    {
+        if (!isFading)
+        {
+            StartCoroutine(Fade(fadeDuration, Color.clear, Color.black));
+        }
+    }
+
+    public void FadeFromBlack(float fadeDuration)
+    {
+        if (!isFading)
+        {
+            StartCoroutine(Fade(fadeDuration, Color.black, Color.clear));
+        }
+    }
+
+    private IEnumerator Fade(float fadeDuration, Color startColor, Color targetColor)
+    {
+        blackScreenImage.enabled = true;
+        isFading = true;
+        float elapsedTime = 0f;
+        Color currentColor = startColor;
+
+        while (elapsedTime < fadeDuration)
+        {
+            currentColor = Color.Lerp(startColor, targetColor, elapsedTime / fadeDuration);
+            blackScreenImage.color = currentColor;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        blackScreenImage.color = targetColor;
+        blackScreenImage.enabled = false;
+        isFading = false;
+    }
+}
