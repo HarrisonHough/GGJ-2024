5ffebc53dbf67225efc0bf745891352d96e53ef3
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-01-28 09:51:28 +0200

    random talk anims

--- Assets/Scripts/PlayerVoice.cs
+++ Assets/Scripts/PlayerVoice.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class PlayerVoice : MonoBehaviour
 {
     private AudioSource audioSource;
+    private Animator animator;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     public void PlayPromptAudio(PromptResponse response)
     {
         audioSource.clip = response.audioClip;
         audioSource.Play();
+        animator.SetTrigger("Talk");
+        animator.SetFloat("TalkIndex", Random.Range(0, 3));
     }
 }
